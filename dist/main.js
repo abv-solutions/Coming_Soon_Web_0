ab939677b66ec3e5254339c1a3dc47d09c2f9545
Andrei Suciu <andreisuciu2005@yahoo.com>
2022-02-06 19:15:53 +0200

    Fix

--- dist/main.js
+++ dist/main.js
@@ -1,32 +1,34 @@
-const countdown = document.querySelector('.countdown');
-const launchDate = new Date('Jan 1, 2021, 12:00:00').getTime();
+const countdown = document.querySelector('.countdown')
+const launchDate = new Date('Jan 1, 2025, 12:00:00').getTime()
 const interval = setInterval(() => {
-  const now = new Date().getTime();
-  const distance = launchDate - now;
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / (1000));
-  countdown.innerHTML = `
+	const now = new Date().getTime()
+	const distance = launchDate - now
+	const days = Math.floor(distance / (1000 * 60 * 60 * 24))
+	const hours = Math.floor(
+		(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+	)
+	const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+	const seconds = Math.floor((distance % (1000 * 60)) / 1000)
+	countdown.innerHTML = `
     <div>${days}<span>Days</span></div>
     <div>${hours}<span>Hours</span></div>
     <div>${mins}<span>Minutes</span></div>
     <div>${seconds}<span>Seconds</span></div>
-  `;
-  if (distance < 0) {
-    clearInterval(interval);
-    countdown.style.color = '#399c11';
-    countdown.style.fontWeight = 'bold';
-    countdown.innerHTML = 'Launched!';
-  }
-}, 1000);
+  `
+	if (distance < 0) {
+		clearInterval(interval)
+		countdown.style.color = '#399c11'
+		countdown.style.fontWeight = 'bold'
+		countdown.innerHTML = 'Launched!'
+	}
+}, 1000)
 
 // Make sure Service Workers are supported
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('sw_cached_site.js')
-      .then(reg => console.log('Service Worker: Registered Pages'))
-      .catch(err => console.log(`Service Worker: Error: ${err}`));
-  });
+	window.addEventListener('load', () => {
+		navigator.serviceWorker
+			.register('../sw_cached_site.js')
+			.then((reg) => console.log('Service Worker: Registered Pages'))
+			.catch((err) => console.log(`Service Worker: Error: ${err}`))
+	})
 }
